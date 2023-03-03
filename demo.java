b14fb9f8335e30e67e83947493dc86ac2799c5f7
Priyanka <priksingal@gmail.com>
2023-03-03 16:10:19 +0000

    DevOps changes commited

--- demo.java
+++ demo.java
@@ -2,5 +2,7 @@ public class demo{
 	public static void main(String args[]){
                  //print statement for hello world
 		System.out.println("Hello World");
+		//print statement for DevOps
+		System.out.println("Welcme to DevOps");
 	}
 }
